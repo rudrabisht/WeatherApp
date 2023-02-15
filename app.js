4a45e05784abd71ad6ce6250f5faa6ec4ee1109a
Rudra Bisht <71915909+rudrabisht@users.noreply.github.com>
2023-02-15 13:44:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
-})
+});
 
 app.post("/",function(req,res){
     const cityName = req.body.cityName;
